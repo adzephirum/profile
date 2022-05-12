62ef9c003af63a63e2689a4dc2bdedf4b8f32da8
dmdgarcia <davidmd.garcia@gmail.com>
2022-05-12 07:30:01 -0400

    edit card

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
 console.log("Hello world");
+
+const counters = document.querySelectorAll(".count");
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute("data-target"));
+    const count = parseInt(counter.innerText);
+    const increment = Math.trunc(target / speed);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
