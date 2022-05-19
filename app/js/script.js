3c49d772d4fe2a09fd65acfc5f7e0952529e9075
dmdgarcia <davidmd.garcia@gmail.com>
2022-05-19 20:35:25 +0900

    edit card

--- app/js/script.js
+++ app/js/script.js
@@ -19,6 +19,12 @@ counters.forEach((counter) => {
   updateCount();
 });
 
+// Pseudocode
+// Get the value we want to animate and transform (origin = 0)
+// animate the value with random Numbers
+// after a specific time this value is changed into the target value.
+// The target value is either an integer or a float following by a "k"
+
 // let counts = setInterval(updated);
 // let upto = 0;
 // function updated() {
