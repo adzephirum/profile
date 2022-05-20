3df6ffaffd3cd8eba8b1e8857b58e14a5e5f9d76
dmdgarcia <davidmd.garcia@gmail.com>
2022-05-20 20:55:13 +0900

    edit card

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,23 @@
 console.log("Hello world");
 
-const counters = document.querySelectorAll(".count");
-const speed = 200;
+// const counters = document.querySelectorAll(".count");
+// const speed = 200;
 
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(counter.getAttribute("data-target"));
-    const count = parseInt(counter.innerText);
-    const increment = Math.trunc(target / speed);
+// counters.forEach((counter) => {
+//   const updateCount = () => {
+//     const target = parseInt(counter.getAttribute("data-target"));
+//     const count = parseInt(counter.innerText);
+//     const increment = Math.trunc(target / speed);
 
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  updateCount();
-});
+//     if (count < target) {
+//       counter.innerText = count + increment;
+//       setTimeout(updateCount, 1);
+//     } else {
+//       counter.innerText = target;
+//     }
+//   };
+//   updateCount();
+// });
 
 // Pseudocode
 // Get the value we want to animate and transform (origin = 0)
