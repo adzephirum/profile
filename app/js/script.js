0805119f70fe986bfb1291b109647088a50b2c7d
dmdgarcia <davidmd.garcia@gmail.com>
2022-09-05 00:06:46 +0900

    edit bg images

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,1 @@
-console.log("Hello world");
-
-// Pseudocode
-// Get the value we want to animate and transform (origin = 0)
-// animate the value with random Numbers
-// after a specific time this value is changed into the target value.
-// The target value is either an integer or a float following by a "k"
+// console.log("Hello world");
